2fc0e8463a6f37ed483ac37abb915a172f4f04de
Metin Mustafa Akkaya <makkaya1@stud.hs-heilbronn.de>
2025-04-08 14:14:21 +0200

    added javadoc

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,23 @@
+/**
+ * This is a simple HelloWorld class that prints "Hello World!" to the console.
+ * It demonstrates basic Java syntax and includes Javadoc comments for documentation.
+ */
 public class HelloWorld {
+
+  /**
+   * A constant integer value used in the class.
+   * This variable is initialized with the value 732.
+   */
   private final int number = 732;
+
+  /**
+   * The main method is the entry point of the program.
+   * It is responsible for printing the "Hello World!" message to the console.
+   *
+   * @param args Command-line arguments (not used in this example).
+   */
   public static void main(String[] args) {
-    System.out.println("Hello World!");
+      // Print "Hello World!" to the console
+      System.out.println("Hello World!");
   }
 }
